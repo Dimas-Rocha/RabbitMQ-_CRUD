f627c0f365b343944117df0456ed3c07dcc4fd8b
Dimas Rocha <dimas.dasp@gmail.com>
2025-10-23 21:40:21 -0300

    Project init

---
+++ src/main/java/Spring_boot_rabbitmq_crud/mq_crud/MqCrudApplication.java
@@ -0,0 +1,13 @@
+package Spring_boot_rabbitmq_crud.mq_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring_boot_rabbitmq_crud/mq_crud/MqCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_boot_rabbitmq_crud.mq_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
