ef6edfaea4bfffa55562bae7662cdfa452702795
Dimas Rocha <dimas.dasp@gmail.com>
2025-10-24 21:44:53 -0300

    UserResponse.DTO.

---
+++ src/main/java/dto/UserResponse.java
@@ -0,0 +1,74 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class UserResponse {
+	private Long id;
+	private String name;
+	private String email;
+	private LocalDateTime createdAt;
+	private LocalDateTime update;
+	
+	
+	//CONSTRUCTORS
+	public UserResponse() {
+		
+	}
+
+	public UserResponse(Long id, String name, String email, LocalDateTime createdAt, LocalDateTime update) {
+		
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.createdAt = createdAt;
+		this.update = update;
+	}
+	//GETTERS AND SETTERS
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdate() {
+		return update;
+	}
+
+	public void setUpdate(LocalDateTime update) {
+		this.update = update;
+	}
+	
+	
+	
+	
+	
+	
+
+}
