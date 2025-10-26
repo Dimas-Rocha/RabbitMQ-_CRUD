4a624059af483b886ff4fa267e152d2155b495ca
Dimas Rocha <dimas.dasp@gmail.com>
2025-10-26 20:58:47 -0300

    UserEventProducer

---
+++ src/main/java/messaging/UserEventProducer.java
@@ -0,0 +1,27 @@
+package messaging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import dto.UserEvent;
+
+@Service
+public class UserEventProducer {
+	private static final Logger logger = LoggerFactory.getLogger(UserEventProducer.class);
+
+	private RabbitTemplate rabbitTemplate;
+
+	public void sendUserEvent(UserEvent event) {
+		try {
+			rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, event);
+			logger.info("Evento enviado para RabbitMQ: {}", event);
+
+		} catch (Exception e) {
+			logger.error("Erro ao enviar evento para RabbitMQ: {}", e.getMessage());
+		}
+
+	}
+
+}
